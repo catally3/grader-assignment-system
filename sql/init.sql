63ae23aba8cc9c9101cefa6ed437e08460af1f42
catally3 <68207926+catally3@users.noreply.github.com>
2025-03-06 16:43:04 -0600

    added to database structure and updated ERD
    I added skills to the database as varchar(50) fields and changed the naming of certain fields to be more consistent. I also updated the ERD and added it as a drawio file and as a svg so that it can be modified (using the .drawio) and easily viewed (using the .svg)

--- sql/init.sql
+++ sql/init.sql
@@ -1,8 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `grader_assignment` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `grader_assignment`;
--- Host: localhost    Database: grader_assignment
--- ------------------------------------------------------
--- Server version	8.0.39
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,29 +11,25 @@ USE `grader_assignment`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `applicant_resume`
+-- Table structure for table `applicant_skills`
 --
 
-DROP TABLE IF EXISTS `applicant_resume`;
+DROP TABLE IF EXISTS `applicant_skills`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `applicant_resume` (
+CREATE TABLE `applicant_skills` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `applicant_net_id` char(10) DEFAULT NULL,
-  `skills` varchar(200) DEFAULT NULL,
+  `skill` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_applicant_id_idx` (`applicant_net_id`),
   CONSTRAINT `fk_applicant_net_id` FOREIGN KEY (`applicant_net_id`) REFERENCES `applicants` (`net_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `applicant_resume`
---
-
-LOCK TABLES `applicant_resume` WRITE;
-/*!40000 ALTER TABLE `applicant_resume` DISABLE KEYS */;
-/*!40000 ALTER TABLE `applicant_resume` ENABLE KEYS */;
+LOCK TABLES `applicant_skills` WRITE;
+/*!40000 ALTER TABLE `applicant_skills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `applicant_skills` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -58,13 +49,11 @@ CREATE TABLE `applicants` (
   `graduation_date` char(10) DEFAULT '2026-05-01',
   `major` varchar(170) DEFAULT 'Computer Science',
   `qualified` tinyint DEFAULT NULL,
+  `continuing` tinyint DEFAULT NULL,
+  `gpa` float unsigned DEFAULT NULL,
   PRIMARY KEY (`net_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `applicants`
---
 
 LOCK TABLES `applicants` WRITE;
 /*!40000 ALTER TABLE `applicants` DISABLE KEYS */;
@@ -80,23 +69,41 @@ DROP TABLE IF EXISTS `assignments`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `assignments` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `applicant_id` char(10) DEFAULT NULL,
+  `applicant_net_id` char(10) DEFAULT NULL,
   `course_id` int unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_prof_id_idx` (`course_id`),
-  KEY `fk_applicant_id_idx` (`applicant_id`),
-  CONSTRAINT `fk_applicant_id` FOREIGN KEY (`applicant_id`) REFERENCES `applicants` (`net_id`),
+  KEY `fk_applicant_id_idx` (`applicant_net_id`),
+  CONSTRAINT `fk_applicant_id` FOREIGN KEY (`applicant_net_id`) REFERENCES `applicants` (`net_id`),
   CONSTRAINT `fk_prof_id` FOREIGN KEY (`course_id`) REFERENCES `professor_courses` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `assignments`
---
-
 LOCK TABLES `assignments` WRITE;
 /*!40000 ALTER TABLE `assignments` DISABLE KEYS */;
 /*!40000 ALTER TABLE `assignments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `course_skills`
+--
+
+DROP TABLE IF EXISTS `course_skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course_skills` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `course_id` int unsigned DEFAULT NULL,
+  `skill` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `prof_course_fk_idx` (`course_id`),
+  CONSTRAINT `prof_course_fk` FOREIGN KEY (`course_id`) REFERENCES `professor_courses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+LOCK TABLES `course_skills` WRITE;
+/*!40000 ALTER TABLE `course_skills` DISABLE KEYS */;
+/*!40000 ALTER TABLE `course_skills` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -118,40 +125,33 @@ CREATE TABLE `professor_courses` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `professor_courses`
---
-
 LOCK TABLES `professor_courses` WRITE;
 /*!40000 ALTER TABLE `professor_courses` DISABLE KEYS */;
 /*!40000 ALTER TABLE `professor_courses` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `recomendations`
+-- Table structure for table `recommendations`
 --
 
-DROP TABLE IF EXISTS `recomendations`;
+DROP TABLE IF EXISTS `recommendations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `recomendations` (
+CREATE TABLE `recommendations` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `professor_course_id` int unsigned DEFAULT NULL,
-  `student_net_id` char(10) DEFAULT 'xxx000000',
+  `professor_id` int unsigned DEFAULT NULL,
+  `applicant_net_id` char(10) DEFAULT 'xxx000000',
   PRIMARY KEY (`id`),
-  KEY `fk_prof_id_idx` (`professor_course_id`),
-  KEY `fk_student_id_idx` (`student_net_id`)
+  KEY `fk_prof_id_idx` (`professor_id`),
+  KEY `fk_student_id_idx` (`applicant_net_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `recomendations`
---
-
-LOCK TABLES `recomendations` WRITE;
-/*!40000 ALTER TABLE `recomendations` DISABLE KEYS */;
-/*!40000 ALTER TABLE `recomendations` ENABLE KEYS */;
+LOCK TABLES `recommendations` WRITE;
+/*!40000 ALTER TABLE `recommendations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recommendations` ENABLE KEYS */;
 UNLOCK TABLES;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -161,4 +161,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
